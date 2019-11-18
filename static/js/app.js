b125941e8e88d94ac6e09d40341392a5815662a1
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2019-11-18 13:14:34 +0530

    Rewrite the whole code
    1. Remove jQuery from the code
    2. Add Axios calls
    3. Add save feature
    4. Add feature to present sets
    5. Make set generation process more readable

--- static/js/app.js
+++ static/js/app.js
@@ -1,97 +1,100 @@
-// eslint-disable-next-line no-undef
-$(document).ready(() => {
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+function startExperiment() {
+  const expData = { user: window.localStorage.getItem('user'), data: null, set: null };
+  // Get data
+  axios
+    .get('/get-exp-data', {
+      user: expData.user,
+    })
+    .then((response) => {
+      console.log(response.data);
+      if (response.data.user === expData.user) {
+        expData.data = response.data.data;
+        expData.set = response.data.set;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    });
   const telugu = [
-    'అ',
-    'న',
-    'వ',
-    'మ',
-    'య',
-    'ల',
-    'ర',
-    'ఒ',
-    'జ',
-    'ఠ',
-    'ఆ',
-    'ఉ',
-    'ఊ',
-    'ఎ',
-    'ఏ',
-    'ప',
-    'ఫ',
-    'ద',
-    'డ',
-    'బ',
-    'త',
-    'క',
-    'హ',
-    'ణ',
+    ['అ', 'న', 'వ', 'మ', 'య', 'ల', 'ర', 'ఒ'],
+    ['జ', 'ఠ', 'ఆ', 'ఉ', 'ఊ', 'ఎ', 'ఏ', 'ప'],
+    ['ఫ', 'ద', 'డ', 'బ', 'త', 'క', 'హ', 'ణ'],
   ];
+  const currentSet = telugu[expData.set].flatMap((shape1, index) => {
+    telugu[expData.set].slice(index + 1).map((shape2) => [shape1, shape2]);
+  });
+
   const next = document.getElementById('next');
   const prev = document.getElementById('prev');
   const btns = document.querySelectorAll('p[name="score"]');
-  let expId = 0;
-  const size = (telugu.length * (telugu.length - 1)) / 2;
-  const diffs = Array(size).fill(0);
+  let expId = expData.data.indexOf(0);
+  if (expId === -1) expId = -5; // To indicate that the exp is done
+  const size = currentSet.length;
 
   function completeExp() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/complete', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(
-      JSON.stringify({
-        value: diffs,
-      }),
-    );
+    axios
+      .post('/complete', {
+        data: expData,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function save(data) {
+    axios
+      .post('/save', { data })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function updateScore(e) {
     document.getElementById('score').innerHTML = e.target.innerHTML;
-    diffs[expId] = +e.target.innerHTML;
+    expData.data[expId] = +e.target.innerHTML;
   }
 
-  function updateScreen(callback) {
-    document.getElementById('score').innerHTML = diffs[expId];
-    const i = Math.floor(expId / size);
-    const j = expId % size;
-    if (i === j) {
-      callback();
-      return;
-    }
-    const letter1 = telugu[i];
-    const letter2 = telugu[j];
-    document.getElementById('letter-1').innerHTML = letter1;
-    document.getElementById('letter-2').innerHTML = letter2;
+  function updateScreen() {
+    document.getElementById('score').innerHTML = expData.data[expId];
+    const shape1 = currentSet[expId][0];
+    const shape2 = currentSet[expId][1];
+    document.getElementById('letter-1').innerHTML = shape1;
+    document.getElementById('letter-2').innerHTML = shape2;
   }
 
   function updateScreenToPrev() {
     expId -= 1;
-    if (expId === 0) expId = 1;
-    document.getElementById('expNo').innerHTML = `Exp ${expId} of 276`;
-    if (diffs[expId] === 0) {
+    if (expId === -1) expId = 0;
+    document.getElementById('expNo').innerHTML = `Exp ${expId + 1} of ${size}`;
+    if (expData.data[expId] === 0) {
       document.getElementById('error').innerHTML = 'Please select a score';
       return;
     }
     document.getElementById('error').innerHTML = '';
-    updateScreen(updateScreenToPrev);
+    save(expData);
+    updateScreen();
   }
 
   function updateScreenToNext() {
-    if (diffs[expId] === 0) {
+    if (expData.data[expId] === 0) {
       document.getElementById('error').innerHTML = 'Please select a score';
       return;
     }
     document.getElementById('error').innerHTML = '';
     expId += 1;
-    document.getElementById('expNo').innerHTML = `Exp ${expId} of 276`;
-    if (expId === diffs.length) completeExp();
-    updateScreen(updateScreenToNext);
-  }
-
-  function startExp() {
-    expId += 1;
-    document.getElementById('expNo').innerHTML = `Exp ${expId} of 276`;
-    if (expId === diffs.length) completeExp();
-    updateScreen(updateScreenToNext);
+    document.getElementById('expNo').innerHTML = `Exp ${expId + 1} of ${size}`;
+    if (expId === size) completeExp();
+    save(expData);
+    updateScreen();
   }
 
   function addEventListner() {
@@ -101,6 +104,12 @@ $(document).ready(() => {
       btns[i].addEventListener('click', updateScore, false);
     }
   }
+
+  // Intialise the experiment page
   addEventListner();
-  startExp();
-});
+  document.getElementById('expNo').innerHTML = `Exp ${expId + 1} of ${size}`;
+  if (expId === -5) completeExp();
+  updateScreen(updateScreenToNext);
+}
+
+window.onload = startExperiment;
