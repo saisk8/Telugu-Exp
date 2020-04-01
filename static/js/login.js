cbde5e586b78486671efe35ec8d019a038d1e9fc
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-01 22:47:35 +0530

    Revert "Remove obsolete js scripts"
    This reverts commit 08ff5c9b76da60caf9082a66a47cf3375f69c5e0.

---
+++ static/js/login.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+window.onload = () => {
+  let apiHost = '';
+  if (window.location.hostname === 'localhost')
+    apiHost = `${window.location.protocol}//${window.location.hostname}:3001/api`;
+  else apiHost = `${window.location.protocol}//${window.location.hostname}/api`;
+
+  function validateEmail(emailField) {
+    const matchString = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return matchString.test(emailField);
+  }
+
+  function login() {
+    const user = document.getElementById('username');
+    if (!validateEmail(user.value)) {
+      user.classList.add('is-invalid');
+      return;
+    }
+    axios
+      .post(`${apiHost}/login-val`, {
+        user: user.value
+      })
+      .then(response => {
+        if (response.data.status) {
+          window.localStorage.setItem('telugu-exp-user', user.value);
+          window.location.href = '/dashboard';
+        } else {
+          window.localStorage.setItem('telugu-exp-temp-user', user.value);
+          window.location.href = '/register';
+        }
+      })
+      .catch(error => {
+        throw error;
+      });
+  }
+  const loginButton = document.getElementById('login');
+  loginButton.addEventListener('click', login, false);
+  document.querySelector('form').addEventListener('keypress', event => {
+    if (event.keyCode === 13) loginButton.click();
+  });
+  document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+  });
+};
